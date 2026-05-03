2f77a754b4ba41596d609ad78905e86fd25c5ecd
jieglee <callmezie23@gmail.com>
2026-05-03 22:26:08 +0700

    middleware-2

--- src/middlewares/Middleware.js
+++ src/middlewares/Middleware.js
@@ -3,7 +3,7 @@ import jwt from "jsonwebtoken"
 import { supabase } from "../config/supabase"
 
 async function Middleware(req, res, next) {
-    const token = req.headers['authorization']
+    const tokenjwt = req.headers['authorization']
 
     if (!tokenjwt) {
         return res.status(401).json({ message: "No Token Provided" })
@@ -18,4 +18,34 @@ async function Middleware(req, res, next) {
         req.user = result
         next()
     })
+}
+
+async function OnlyOwnerReport(req, res, next) {
+    try {
+    const userId = req.user.userId
+    const reportId = req.params.id
+
+    const { data, error } = await supabase
+    .from("reports")
+    .select("*")
+    .eq("id", reportId)
+    .eq("user_id", userId)
+
+    if (!data.length) {
+        return res.status(404).json ({ message: "Data Not Found" })
+    }
+
+    if (req.user.role === "superadmin"){
+        return next ()
+    }
+
+    if (data.user_id !== userId) {
+        return res.status(403).json ({ message: "Not Your Data" })
+    }
+    next()
+
+    } catch (error) {
+        return req.status(500).json ({ message: error.message })
+
+    }
 }
